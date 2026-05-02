091e1b3768bdb7f351dc81bab913b5b69f439028
Bella Kreit <Bella@kreit.net>
2026-05-02 01:36:04 +0000

    most popular page update

--- OraWigs_app.py
+++ OraWigs_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 # streamlit run OraWigs_app.py to run app
 Home_page = st.Page("Home.py", title="Home Page")  # creat home page
-Shop_page = st.Page("Shop.py", title="Shop Page")  # create shop page
+PopularWig_page = st.Page("PopularWig.py", title="Most Popular Wig Page")  # create popular wig page
 Compare_page = st.Page("Compare.py", title="Compare Page")  # create compare page
 Order_page = st.Page("Order.py", title="Order Page")  # create order page
 Tutorial_page = st.Page("Tutorial.py", title="Tutorial Page")  # create tutorial page
 HairColor_page = st.Page("HairColor.py", title="Hair Color Page")  # create hair color page
-pg = st.navigation([Home_page, Shop_page, Compare_page, Order_page, Tutorial_page, HairColor_page])  # make a navigation for the pages
+pg = st.navigation([Home_page, PopularWig_page, Compare_page, Order_page, Tutorial_page, HairColor_page])  # make a navigation for the pages
 st.set_page_config(page_title="Ora Wigs")  # keep browser consistent
 pg.run()
--- PopularWig.py
+++ PopularWig.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 import plotly.express as px
 
-st.title("Shop Page")
+st.title("Most Popular Wig Page")
 
 st.markdown("Our most popular wig based on our purchases right now is...")
 conn2 = sqlite3.connect('OraWigs.db')  # open connection
