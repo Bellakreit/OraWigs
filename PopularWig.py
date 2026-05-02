df52503d1c16f8d7371a51768de6322fcc78b0b3
Bella Kreit <Bella@kreit.net>
2026-05-02 01:37:38 +0000

    change title

--- PopularWig.py
+++ PopularWig.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 import plotly.express as px
 
-st.title("Most Popular Wig Page")
+st.title("Wig Popularity")
 
 st.markdown("Our most popular wig based on our purchases right now is...")
 conn2 = sqlite3.connect('OraWigs.db')  # open connection
